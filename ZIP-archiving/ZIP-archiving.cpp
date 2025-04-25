05030ce511a94613d307b4d45d5ccf0649c003bd
LordReaps <BovenkoAndrey@gmail.com>
2025-04-25 14:49:31 +0300

    create functions:

--- ZIP-archiving/ZIP-archiving.cpp
+++ ZIP-archiving/ZIP-archiving.cpp
@@ -2,7 +2,120 @@
 #include <fstream>
 #include <windows.h>
 #include <string>
+#include <vector>
+#include <utility>
 using namespace std;
+
+struct Node {
+    char letter;
+    int freq;
+    Node* l, * r;
+
+    Node(char l, int f) :
+        letter(l),
+        freq(f),
+        l(nullptr),
+        r(nullptr){}
+};
+
+
+//структура минимальной кучи
+struct Min_Stack{
+    int size;
+    vector<Node*> stack;
+
+    Min_Stack(int s) : size(s), stack(s) {}
+};
+
+//Реализация Heapify (функция для создания кучи)
+void Heapify(Min_Stack* heap, int i) {
+    int smallest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < heap->size && heap->stack[left]->freq < heap->stack[smallest]->freq)
+        smallest = left;
+
+    if (right < heap->size && heap->stack[right]->freq < heap->stack[smallest]->freq)
+        smallest = right;
+
+    if (smallest != i) {
+        swap(heap->stack[i], heap->stack[smallest]);
+        Heapify(heap, smallest);
+    }
+}
+
+//Создание минимальной кучи
+Min_Stack* CreateMin_stack(char arr[], int freq[], int uniq_size)
+{
+    int i;
+
+    Min_Stack* Heap = new Min_Stack(uniq_size);
+
+    //Создаем массив указателей в минимальной куче
+    //Указатели указывают на следующие элементы и частоту
+
+    for (i = 0; i < uniq_size; ++i) {
+        Heap->stack[i] = new Node(arr[i], freq[i]);
+    }
+
+    int n = Heap->size - 1;
+    for (i = (n - 1) / 2; i >= 0; --i) {
+        Heapify(Heap, i);
+    }
+
+    return Heap;
+}
+
+//Создает новый узел с заданным символом и частотой
+Node* newNode(char letter, int freq) {
+    Node* node = new Node(letter, freq);
+    node->l = nullptr;
+    node->r = nullptr;
+    return node;
+}
+
+Node* extractMinFrom_Heap(Min_Stack* heap) {
+    if (heap->size == 0) return nullptr;
+    //Берём корень (минимальный элемент)
+    Node* minNode = heap->stack[0];
+    // Ставим последний элемент на место корня
+    heap->stack[0] = heap->stack[heap->size - 1];
+    heap->size--;
+    // Восстанавливаем кучу
+    Heapify(heap, 0);
+
+    return minNode;
+}
+
+//Вставка элемента в кучу
+void insertInto_Heap(Min_Stack* heap, Node* node) {
+    heap->size++;
+    int i = heap->size - 1;
+
+    // Поднимаем узел вверх, пока он меньше родителя
+    while (i > 0 && node->freq < heap->stack[(i - 1) / 2]->freq) {
+        heap->stack[i] = heap->stack[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+    heap->stack[i] = node;
+}
+
+//Создание дерева Хаффмана
+struct Node* CreateHuffmanTree(char arr[], int freq[],
+    int uniq_size, Min_Stack* Heap) {
+    struct Node *l, *r, *top;
+    while (!(Heap->size == 1)) {
+        l = extractMinFrom_Heap(Heap);
+        r = extractMinFrom_Heap(Heap);
+        top = newNode('$', l->freq + r->freq);
+        top->l = l;
+        top->r = r;
+        insertInto_Heap(Heap, top);
+    }
+
+    return extractMinFrom_Heap(Heap);
+}
 
 int main()
 {
@@ -23,7 +136,7 @@ int main()
         istreambuf_iterator<char>());
     
     cout << text << endl;
-    in.close();
+
 
 
     //Открытие файла на запись закодированного текста
@@ -34,9 +147,8 @@ int main()
         cerr << "Не удалось открыть файл для записи!" << endl;
         return 1;
     }
-    
-    file << "Hello";
-    file << 1232322222222222;
+
+    in.close();
     file.close();
 
     return 0;
